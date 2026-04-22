cc03d698abc3aab32f0a425d97d32acd2d7a9a7e
JD <jaye.personal.acc@gmail.com>
2026-04-23 07:12:52 +0900

    paste

--- library/intime-scripts.js
+++ library/intime-scripts.js
@@ -1,1 +1,15 @@
+    const pomodorobtn=document.getElementsByClassName("pilled-op-pom");
+    const analyticsbtn=document.getElementsByClassName("pilled-op-ana");
+    const analytics=document.getElementsByClassName("analytics");
 
+    function pombtn(){
+        pomodorobtn[0].classList.add("focused_pilled-option-btn");
+        analyticsbtn[0].classList.remove("focused_pilled-option-btn");
+        analytics[0].style.display="none";
+    }
+    function anabtn(){
+        pomodorobtn[0].classList.remove("focused_pilled-option-btn");
+        analyticsbtn[0].classList.add("focused_pilled-option-btn");
+        analytics[0].style.display="flex";
+    }
+    
